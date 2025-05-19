f4cc9b479f338011d18e5ad2d674cc3eda1dc6a4
Chaitanya Khandekar <chaitanyakhandekar95@gmail.com>
2025-05-19 21:37:41 +0530

    learned about routes and postman

--- Express js/5 Production grade backend setup (for mega project)/src/app.js
+++ Express js/5 Production grade backend setup (for mega project)/src/app.js
@@ -13,4 +13,17 @@ server.use(express.json({limit:"16kb"}))
 server.use(express.urlencoded({extended:true, limit:"16kb"}))
 server.use(express.static("public"))
 server.use(cookieParser())
+
+
+
+//best practice to import Routes here instead of index.js
+
+//Import 
+
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+
+server.use("/api/v1/users", userRouter)
+
 export {server}
