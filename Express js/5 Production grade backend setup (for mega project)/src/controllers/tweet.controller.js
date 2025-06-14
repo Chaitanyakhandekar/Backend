6a9ab2b47b5afc25c827c6436bdc2400e23b786a
Chaitanya Khandekar <chaitanyakhandekar95@gmail.com>
2025-06-14 18:05:30 +0530

    just completed tweet controller. maybe update in future

--- Express js/5 Production grade backend setup (for mega project)/src/controllers/tweet.controller.js
+++ Express js/5 Production grade backend setup (for mega project)/src/controllers/tweet.controller.js
@@ -142,7 +142,127 @@ const getTweetById = asyncHandler(async (req,res)=>{
                 )
 })
 
-const getAllTweets = asyncHandler(async (req,res)=>{    // not started yet
+const getAllTweets = asyncHandler(async (req,res)=>{ 
+
+        const page = Number(req.query.page) || 1 
+        const limit = Number(req.query.limit) || 10
+        const skip = (page - 1)*limit
+
+        const totalTweets = await Tweet.countDocuments()
+
+        const tweets = await Tweet.aggregate([
+            {
+                $sort:{createdAt:-1}
+            },
+            {
+                $skip:skip
+            },
+            {
+                $limit:limit
+            },
+            {
+                $lookup:{
+                    from:"users",
+                    localField:"owner",
+                    foreignField:"_id",
+                    as:"owner"
+                }
+            },
+            {
+                $addFields:{
+                    owner:{
+                        $first:"$owner"
+                    }
+                }
+            },
+            {
+                $project:{
+                    owner:{
+                        _id:1,
+                        username:1,
+                        email:1,
+                        fullName:1,
+                        avatar:1
+                    },
+                    content:1
+                }
+            }
+           
+        ])
+
+        if(!tweets){
+            throw new ApiError(404,"Twees not found")
+        }
+
+        return res
+                .status(200)
+                .json(
+                    new ApiResponse(200,{
+                        tweets,
+                        page,
+                        limit,
+                        skip,
+                        totalTweets,
+                        totalPages:Math.ceil(totalTweets / limit),
+                        hasMore:page * limit < totalTweets
+                    }),
+                    "Tweets fetched Successfully"
+                )
+
+})
+
+const getUserTweets = asyncHandler(async (req,res)=>{
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+
+    const totalUserTweets = await Tweet.countDocuments({
+        owner:req.user._id
+    })
+
+    const userTweets = await Tweet.aggregate([
+        {
+            $match:{
+                owner:new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $sort:{createdAt:-1}
+        },
+        {
+            $skip:skip
+        },
+        {
+            $limit:limit
+        },
+        {
+            $project:{
+                _id:1,
+                content:1,
+                createdAt:1,
+                updatedAt:1
+            }
+        }
+    ])
+
+    if(!userTweets.length){
+        throw new ApiError(404,"Tweets not found!")
+    }
+
+    return res
+            .status(200)
+            .json(
+                new ApiResponse(200,{
+                    userTweets,
+                    page:page,
+                    limit:limit,
+                    totalUserTweets,
+                    totalPages:Math.ceil(totalUserTweets / limit),
+                    hasMore: page * limit < totalUserTweets
+                },
+                "User Tweets fetched Successfully")
+            )
 
 })
 
@@ -152,4 +272,5 @@ export {
     getTweetById,
     deleteTweet,
     getAllTweets,
+    getUserTweets
 }
